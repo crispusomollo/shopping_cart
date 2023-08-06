1715c40d231ebea4ed1ec4d23c1f01ccb28da3fb
Crispus Makokha Omollo <126664434+crispusomollo@users.noreply.github.com>
2023-08-06 18:23:15 +0300

    Update cart.py

--- cart.py
+++ cart.py
@@ -1,44 +1,43 @@
-cart = {} #Dictionary
-print("Welcome to the Shopping Cart Program!")
+cart = {}
+print("This is a Shopping Cart!")
  
 while True:
     print()
-    print ("Please type in one of these options")
-    print ("1. Add item ")
-    print ("2. View cart ")
-    print ("3. Remove item ")
-    print ("4. Compute total ")
-    print ("5. Quit application ")
+    print ("Please type in one of these options: ")
+    print ("1. Add an item ")
+    print ("2. View your cart ")
+    print ("3. Remove an item: ")
+    print ("4. Calculate total: ")
+    print ("5. Quit the application ")
   
     print()
-    select = int(input(" Type in a number to go on "))
+    option = int(input(" Type in a number to proceed "))
     print()
  
-    if select == 1:
-        item = input(" What would you like to add?  ")
-        price = float(input(" Type in the price "))
+    if option == 1:
+        item = input(" What would you like to add in your cart?  ")
+        price = float(input("Type in the price of the item added: "))
         cart [item] = price
-        print(f" {item} costing ${price} has been added to your cart.")
-        #print(f" The price of {item} is ${price} ")
+        print(f" {item} costing ${price} has been added successfully to your cart.")
  
-    if select == 2:
-        print("This is what is in your shopping cart")
+    if option == 2:
+        print("These are the items in your shopping cart: ")
         for item in cart:
             print(f"  {item} - ${cart [item]}")
  
-    if select == 3:
-        takeout = input(" Type in what you would like to remove? ")
-        cart.pop(takeout)
-        print(f" {takeout} has been removed from your cart" )
+    if option == 3:
+        remove = input(" Type in the item you would like to remove from your cart: ")
+        cart.pop(remove)
+        print(f" {remove} has been removed from your cart" )
  
-    if select == 4:
+    if option == 4:
         total = 0
         for item in cart:
             total += cart [item]
         print(f" The total cost of items in the cart is ${total}")
  
-    if select == 5:
-        print ("Comeback soon.")
+    if option == 5:
+        print ("Thank you for visiting my cart.")
         #quit()
         break
 
