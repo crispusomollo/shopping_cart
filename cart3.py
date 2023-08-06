a19d36636360d4ceb47195589f33d26e048c11d7
crispusomollo <crispusomollo@gmail.com>
2023-08-06 18:10:11 +0300

    Cart updated

--- cart3.py
+++ cart3.py
@@ -3,21 +3,23 @@ print("Welcome to the Shopping Cart Program!")
  
 while True:
     print()
-    print ("Please type in one of these")
+    print ("Please type in one of these options")
     print ("1. Add item ")
     print ("2. View cart ")
-    print ("3. Remove Item ")
-    print ("4. compute total")
-    print ("5. exit program")
+    print ("3. Remove item ")
+    print ("4. Compute total ")
+    print ("5. Quit application ")
   
+    print()
     select = int(input(" Type in a number to go on "))
+    print()
  
     if select == 1:
         item = input(" What would you like to add?  ")
-        price = float(input(" type in the price "))
+        price = float(input(" Type in the price "))
         cart [item] = price
-        print(f"'{item}' has been added to your cart.")
-        print(f" The price is ${price}")
+        print(f" {item} costing ${price} has been added to your cart.")
+        #print(f" The price of {item} is ${price} ")
  
     if select == 2:
         print("This is what is in your shopping cart")
@@ -25,15 +27,18 @@ while True:
             print(f"  {item} - ${cart [item]}")
  
     if select == 3:
-        takeout = input(" Type in what you would like to remove?  ")
+        takeout = input(" Type in what you would like to remove? ")
         cart.pop(takeout)
+        print(f" {takeout} has been removed from your cart" )
  
     if select == 4:
         total = 0
         for item in cart:
             total += cart [item]
-        print(f" ${total}")
+        print(f" The total cost of items in the cart is ${total}")
  
     if select == 5:
-        print ("comeback soon.")
+        print ("Comeback soon.")
+        #quit()
         break
+
